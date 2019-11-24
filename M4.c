9e5428d44f7da5a0d00f4f7a530c0e34df665397
Zubin Jimmy Paymaster <zubinpaymaster@yahoo.co.in>
2019-11-24 12:39:27 +0530

    Create M4.c

---
+++ M4.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <math.h>
+#include "mpi.h"
+
+double func(double x)
+{
+    return (4.0 / (1.0 + x*x));
+}
+
+int main(int argc,char *argv[])
+{
+    int    NoInterval, interval;
+    int    MyRank, Numprocs, Root = 0; 
+    double mypi, pi, h, sum, x;
+    double PI25DT = 3.141592653589793238462643;
+
+    /*....MPI initialisation....*/
+    MPI_Init(&argc,&argv);
+    MPI_Comm_size(MPI_COMM_WORLD,&Numprocs);
+    MPI_Comm_rank(MPI_COMM_WORLD,&MyRank);
+
+    if(MyRank == Root){
+	printf("\nEnter the number of intervals : ");
+	scanf("%d",&NoInterval);
+    }
+        
+   /*....Broadcast the number of subintervals to each processor....*/ 
+   MPI_Bcast(&NoInterval, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+   if(NoInterval <= 0){
+      if(MyRank == Root) 
+	 printf("Invalid Value for Number of Intervals .....\n");
+      MPI_Finalize();
+      exit(-1);
+   }
+
+   h   = 1.0 / (double)NoInterval;
+   sum = 0.0;
+   for(interval = MyRank + 1; interval <= NoInterval; interval += Numprocs){
+       x = h * ((double)interval - 0.5);
+       sum += func(x);
+   }
+   mypi = h * sum;
+
+   /*....Collect the areas calculated in P0....*/ 
+   MPI_Reduce(&mypi, &pi, 1, MPI_DOUBLE, MPI_SUM, Root, MPI_COMM_WORLD);
+
+   if(MyRank == Root){
+      printf("pi is approximately %.16f, Error is %.16f\n", 
+						   pi, fabs(pi - PI25DT));
+   }
+
+   MPI_Finalize();
+
+}
