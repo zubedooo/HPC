7ca6dac19ecb09c15f58b116bbc26a69202dd8d6
Zubin Jimmy Paymaster <zubinpaymaster@yahoo.co.in>
2019-12-12 20:26:35 +0530

    Update 7b.c

--- 7b.c
+++ 7b.c
@@ -1,14 +1,10 @@
-#include   <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include   <mpi.h>
-
+#include<stdio.h>
+#include<mpi.h>
+  
   void main(int argc, char *argv[])
   {
 	int rank,size;
-	char param[10][10];
-	char mine[10];
-	char srank[10];
+	double param[6],mine;
 	int sndcnt,rcvcnt;
 	int i;
 
@@ -16,21 +12,17 @@
 	MPI_Comm_rank(MPI_COMM_WORLD,&rank);
 	MPI_Comm_size(MPI_COMM_WORLD,&size);
 
-	sndcnt=10;
-	strcpy(mine,"Hello ");
-	//srank = itoa(rank,srank,10);
-	sprintf(srank, "%d", rank);
-	strcat(mine,srank);
+	sndcnt=1;
+	mine=23.0+rank;
+	if(rank==0) rcvcnt=1;
 
-	if(rank==0) rcvcnt=10;
-
-	MPI_Gather(&mine,sndcnt,MPI_CHAR,param,rcvcnt,MPI_CHAR,0,MPI_COMM_WORLD);
+	MPI_Gather(&mine,sndcnt,MPI_DOUBLE,param,rcvcnt,MPI_DOUBLE,0,MPI_COMM_WORLD);
 
 	if(rank==0)
-    	for(i=0;i<size;++i)
-     	{
-     	printf("PE:%d param[%d] is %s \n",rank,i,param[i]);
-     	printf(" %d %d  \n",rank,i);
-    	}
+		for(i=0;i<size;++i) 
+		 {
+		 printf("PE:%d param[%d] is %f \n",rank,i,param[i]); 
+		 printf(" %d %d  \n",rank,i); 
+		}
 	MPI_Finalize();
 }
