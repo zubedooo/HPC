c91532ddc86621b4c75431b1795a243a0604f423
Zubin Jimmy Paymaster <zubinpaymaster@yahoo.co.in>
2019-12-12 20:30:10 +0530

    Update 1b.cu

--- 1b.cu
+++ 1b.cu
@@ -1,23 +1,18 @@
 #include <stdio.h>
-
-#define NUM_BLOCKS 32
-#define BLOCK_WIDTH 1
+#define NUM_BLOCKS 1
+#define BLOCK_WIDTH 64
 
 __global__ void hello()
 {
-    printf("Hello world! I'm a thread in block %d\n", blockIdx.x);
+    printf("Hello world! I'm thread %d\n", threadIdx.x);
 }
-
 
 int main(int argc,char **argv)
 {
     // launch the kernel
     hello<<<NUM_BLOCKS, BLOCK_WIDTH>>>();
-
-    // force the printf()s to flush
+// force the printf()s to flush
     cudaDeviceSynchronize();
-
     printf("That's all!\n");
-
     return 0;
 }
