4eaa401ab233fe27ba98d950db0c52cc57140056
Zubin Jimmy Paymaster <zubinpaymaster@yahoo.co.in>
2019-12-20 00:28:52 +0530

    Update 4b.c

--- 4b.c
+++ 4b.c
@@ -1,39 +1,49 @@
-#include <mpi.h>
-#include<stdio.h>
-#include<math.h>
+#include <stdio.h>
+#include <math.h>
+#include "mpi.h"
 
 double func(double x)
-{return (4.0 / (1.0 + x*x));
+{
+    return (4.0 / (1.0 + x*x));
 }
 int main(int argc,char *argv[])
 {
-	int n,i,rank,size;
-	double mypi, pi, h, sum, x;
-	double PI25DT = 3.141592653589793238462643;
+    int    NoInterval, interval;
+    int    MyRank, Numprocs, Root = 0; 
+    double mypi, pi, h, sum, x;
+    double PI25DT = 3.141592653589793238462643;
 
-	MPI_Init(&argc,&argv);
-	MPI_Comm_size(MPI_COMM_WORLD,&size);
-	MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+    MPI_Init(&argc,&argv);
+    MPI_Comm_size(MPI_COMM_WORLD,&Numprocs);
+    MPI_Comm_rank(MPI_COMM_WORLD,&MyRank);
 
-	if(rank == 0){
+    if(MyRank == Root){
 	printf("\nEnter the number of intervals : ");
-	scanf("%d",&n);
-	}
-    	 
-   MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
+	scanf("%d",&NoInterval);
+}
+    
+   MPI_Bcast(&NoInterval, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
+   if(NoInterval <= 0){
+      if(MyRank == Root) 
+	 printf("Invalid Value for Number of Intervals .....\n");
+      MPI_Finalize();
+      exit(-1);
+   }
 
-   h   = 1.0 / (double)n;
+   h   = 1.0 / (double)NoInterval;
    sum = 0.0;
-   for(i = rank + 1; i <= n; i +=size){
-   	x = h * ((double)i - 0.5);
-   	sum += func(x);
+   for(interval = MyRank + 1; interval <= NoInterval; interval += Numprocs){
+       x = h * ((double)interval - 0.5);
+       sum += func(x);
    }
    mypi = h * sum;
-   MPI_Reduce(&mypi, &pi, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
-   if(rank == 0){
-  	printf("pi is approximately %.16f, Error is %.16f\n",
-pi, fabs(pi - PI25DT));
+   MPI_Reduce(&mypi, &pi, 1, MPI_DOUBLE, MPI_SUM, Root, MPI_COMM_WORLD);
+
+   if(MyRank == Root){
+      printf("pi is approximately %.16f, Error is %.16f\n",pi, fabs(pi - PI25DT));
    }
- MPI_Finalize();
+	MPI_Finalize();
+
 }
+
