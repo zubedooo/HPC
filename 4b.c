5f2ed54f133cbad35a5087a37bbd6a5f902fe059
Zubin Jimmy Paymaster <zubinpaymaster@yahoo.co.in>
2019-12-20 00:14:50 +0530

    Update 4b.c

--- 4b.c
+++ 4b.c
@@ -1,55 +1,45 @@
-#include <stdio.h>
-#include <math.h>
-#include "mpi.h"
+#include <mpi.h>
+#include<stdio.h>
+#include<math.h>
 
 double func(double x)
 {
-    return (4.0 / (1.0 + x*x));
+	return (4.0 / (1.0 + x*x));
 }
 
 int main(int argc,char *argv[])
 {
-    int    NoInterval, interval;
-    int    MyRank, Numprocs, Root = 0; 
-    double mypi, pi, h, sum, x;
-    double PI25DT = 3.141592653589793238462643;
+	int	n,i,rank,size;
+	double mypi, pi, h, sum, x;
+	double PI25DT = 3.141592653589793238462643;
 
-    /*....MPI initialisation....*/
-    MPI_Init(&argc,&argv);
-    MPI_Comm_size(MPI_COMM_WORLD,&Numprocs);
-    MPI_Comm_rank(MPI_COMM_WORLD,&MyRank);
+	MPI_Init(&argc,&argv);
+	MPI_Comm_size(MPI_COMM_WORLD,&size);
+	MPI_Comm_rank(MPI_COMM_WORLD,&rank);
 
-    if(MyRank == Root){
+	if(rank == 0){
 	printf("\nEnter the number of intervals : ");
-	scanf("%d",&NoInterval);
-    }
-        
-   /*....Broadcast the number of subintervals to each processor....*/ 
-   MPI_Bcast(&NoInterval, 1, MPI_INT, 0, MPI_COMM_WORLD);
+	scanf("%d",&n);
+	}
+    	 
+   MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
-   if(NoInterval <= 0){
-      if(MyRank == Root) 
-	 printf("Invalid Value for Number of Intervals .....\n");
-      MPI_Finalize();
-      exit(-1);
-   }
 
-   h   = 1.0 / (double)NoInterval;
+   h   = 1.0 / (double)n;
    sum = 0.0;
-   for(interval = MyRank + 1; interval <= NoInterval; interval += Numprocs){
-       x = h * ((double)interval - 0.5);
-       sum += func(x);
+   for(i = rank + 1; i <= n; i +=size){
+   	x = h * ((double)i - 0.5);
+   	sum += func(x);
    }
    mypi = h * sum;
 
-   /*....Collect the areas calculated in P0....*/ 
-   MPI_Reduce(&mypi, &pi, 1, MPI_DOUBLE, MPI_SUM, Root, MPI_COMM_WORLD);
+   
+   MPI_Reduce(&mypi, &pi, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
 
-   if(MyRank == Root){
-      printf("pi is approximately %.16f, Error is %.16f\n", 
-						   pi, fabs(pi - PI25DT));
+   if(rank == 0){
+  	printf("pi is approximately %.16f, Error is %.16f\n",
+pi, fabs(pi - PI25DT));
    }
 
    MPI_Finalize();
-
 }
