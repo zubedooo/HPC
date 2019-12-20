0cd5581c4dbfd408ce9d2b1777622d78a2fa2cd4
Zubin Jimmy Paymaster <zubinpaymaster@yahoo.co.in>
2019-12-20 07:19:52 +0530

    Update 14a.c

--- 14a.c
+++ 14a.c
@@ -10,19 +10,10 @@ int main (int argc, char *argv[])
 {
 int
 tid, nthreads, i, j, k, chunk;
-double a[NRA][NCA],
-/* matrix A to be multiplied */
-b[NCA][NCB],
-/* matrix B to be multiplied */
-c[NRA][NCB];
-/* result matrix C */
+double a[NRA][NCA],b[NCA][NCB],c[NRA][NCB];
 
-/* number of rows in matrix A */
-/* number of columns in matrix A */
-/* number of columns in matrix B */
 chunk = 10;
 
-/*** Spawn a parallel region explicitly scoping all variables ***/
 #pragma omp parallel shared(a,b,c,nthreads,chunk) private(tid,i,j,k)
 {
 tid = omp_get_thread_num();
@@ -32,7 +23,7 @@ nthreads = omp_get_num_threads();
 printf("Starting matrix multiple example with %d threads\n",nthreads);
 printf("Initializing matrices...\n");
 }
-/*** Initialize matrices ***/
+
 #pragma omp for schedule (static, chunk)
 for (i=0; i<NRA; i++)
 for (j=0; j<NCA; j++)
@@ -46,8 +37,6 @@ for (i=0; i<NRA; i++)
 for (j=0; j<NCB; j++)
 c[i][j]= 0;
 
-/*** Do matrix multiply sharing iterations on outer loop ***/
-/*** Display who does which iterations for demonstration purposes ***/
 printf("Thread %d starting matrix multiply...\n",tid);
 #pragma omp for schedule (static, chunk)
 for (i=0; i<NRA; i++)
@@ -57,11 +46,8 @@ for(j=0; j<NCB; j++)
 for (k=0; k<NCA; k++)
 c[i][j] += a[i][k] * b[k][j];
 }
-} /*** End of parallel region ***/
+}
 
-/*** Print results ***/
-
-/* set loop iteration chunk size */
 printf("******************************************************\n");
 printf("Result Matrix:\n");
 for (i=0; i<NRA; i++)
